b260e37adc8fb879042c22ac3dc325a64786ad70
Marcy Silverman <marcysilve225@gmail.com>
2019-03-06 11:07:43 -0800

    changed reducer

--- src/reducers/simpsons.js
+++ src/reducers/simpsons.js
@@ -1,9 +1,22 @@
+import { FETCH_QUOTES, FETCH_QUOTES_LOADING } from "../action/simpsons";
+
 const initialState = {
-  quotes: '',
-  name: '',
-  image: ''
+  quotes: [],
+  loading: false
 }
 
 export default function reducer(state = initialState, { type, payload}) {
-  return state;
+  switch(type) {
+    case FETCH_QUOTES:
+      return { ...state,
+      quotes: payload,
+      loading: false
+    };
+    case FETCH_QUOTES_LOADING:
+    return { ...state,
+      loading: true
+      };
+    default:
+      return state;
+  }
 }
