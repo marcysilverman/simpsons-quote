95ce126c1ccc7cab30d79d8a6e8c403460257196
Marcy Silverman <marcysilve225@gmail.com>
2019-03-06 20:56:07 -0800

    picture and name is showing

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Quote({ quote }) {
+function Quote({ quote, name, image }) {
   return (
     <>
     <h1> {quote} </h1>
+    <h1> {name} </h1>
+    <img src={image}/>
     </>
   );
 }
 
 Quote.propTypes = {
-  quote: PropTypes.string.isRequired
+  quote: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
 };
 
 export default Quote;
--- src/containers/quote/SimpsonsQuote.js
+++ src/containers/quote/SimpsonsQuote.js
@@ -2,13 +2,15 @@ import { connect } from 'react-redux';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Quote from '../../components/quote/Quote';
-import { getQuote, isLoading } from '../../selector/simpson';
+import { getQuote, isLoading, getCharacterName, getCharacterImage } from '../../selector/simpson';
 import { fetchQuotes } from '../../action/simpsons';
 
 
 class SimpsonQuote extends PureComponent {
   static propTypes = {
     quote: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
     loading: PropTypes.bool.isRequired,
     fetch: PropTypes.func.isRequired
   };
@@ -27,6 +29,8 @@ class SimpsonQuote extends PureComponent {
 
 const mapStateToProps = (state) => ({
   quote: getQuote(state),
+  name: getCharacterName(state),
+  image: getCharacterImage(state),
   loading: isLoading(state)
 });
 
