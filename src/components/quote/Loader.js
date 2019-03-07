7888fcbaaff97d29518b6d1143d6d692640adee6
Marcy Silverman <marcysilve225@gmail.com>
2019-03-06 22:17:01 -0800

    added animation and css

--- src/components/quote/Loader.js
+++ src/components/quote/Loader.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { pulse } from 'react-animations';
+import Radium, { StyleRoot } from 'radium';
+
+const animation = {
+  pulse: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(pulse, 'pulse')
+  }
+}
 
 function Loader({ fetch }) {
   return (
-    <button onClick={fetch}> Get Another Quote </button>
+    <StyleRoot>
+    <button onClick={fetch} style={animation.pulse}> Get Another Character </button>
+    </StyleRoot>
   );
 }
 
--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -2,17 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Loader from './Loader';
 import styles from './quote.css';
+import { fadeIn } from 'react-animations';
+import Radium, { StyleRoot } from 'radium';
+
+const fade = {
+  fadeIn: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeIn, 'fadeIn')
+  }
+}
 
 function Quote({ quote, name, image, loading, fetch }) {
   return (
     <>
-    {loading ? <h1 className={styles.homeTitle}>Donuts...Yum<img className={styles.image} src='../../../assets/loader.png'/></h1> : 
-    <div>
+    <StyleRoot>
+    {loading ? <h1 className={styles.homeTitle}>Donuts...Yum<img className={styles.image} src='https://ih0.redbubble.net/image.232274915.1078/sticker,375x360-bg,ffffff.u6.png'/></h1> : 
+    <div style={fade.fadeIn}>
       <h1 className={styles.homeName}> {name} </h1>
       <h1 className={styles.homeQuote}> {quote} </h1>
       <img className={styles.homeImage} src={image}/>
       <Loader fetch={fetch} /> 
     </div>}
+    </StyleRoot>
     </>
   );
 }
